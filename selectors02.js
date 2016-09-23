e072f265af7a0962ac2cb69326876ced92eb04fe
DallinDansie <dansiedallin@gmail.com>
2016-09-23 08:18:10 -0600

    Add files via upload

--- selectors02.js
+++ selectors02.js
@@ -1,14 +1,20 @@
 $(document).ready(function() {
 
+	$("#buttonpic").hide();
 	$("#testbutton").click(function() {
-		
-/*
-		$("#selector02").css("background-color", "red");
 
-		
-		$("h3, #testbutton").css("background-color", "red");
-		$("*").css("background-color", "red");
-		*/
-		
+		$("#buttonpic").fadeToggle(500);
+
+		/*
+		 $("#selector02").css("background-color", "red");
+		 $("h3, #testbutton").css("background-color", "red");
+		 $("*").css("background-color", "red");
+		 */
+
 	});
-}); 
+	
+	$("#aboutmebutton").click(function(){
+		
+		$("#aboutmeinfo").fadeToggle(500);
+	});
+});
