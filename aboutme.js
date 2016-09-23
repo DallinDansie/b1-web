a2dca0769def4102c3873a9f4b8e42d736712fd8
DallinDansie <dansiedallin@gmail.com>
2016-09-23 08:11:07 -0600

    Add files via upload

---
+++ aboutme.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+	$("#aboutmeinfo").hide();
+	/*We need to create this */
+
+	$("#aboutmebutton").click(function() {
+
+		$("#aboutmeinfo").slideToggle(500);
+
+	});
+});
